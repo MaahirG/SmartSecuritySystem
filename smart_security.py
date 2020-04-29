15f1f2e624c069829080f8be9c7d460d9dca8fb1
Maahir <maahir.gupta@gmail.com>
2020-04-29 18:53:34 -0400

    Haar detection + general motion detection

--- smart_security.py
+++ smart_security.py
@@ -8,6 +8,10 @@ import datetime
 import imutils
 import time
 import cv2
+import numpy
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # reading from webcam
 vs = VideoStream(src=0).start()
@@ -18,10 +22,23 @@ firstFrame = None
 
 # loop over the frames of the video
 while True:
-	# grab the current frame and initialize the occupied/unoccupied
-	# text
+	# grab the current frame and initialize the occupied/unoccupied text
 	frame = vs.read()
 	text = "Unoccupied"
+	
+	forHaar = frame
+	grayHaar = cv2.cvtColor(forHaar, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(grayHaar, 1.3, 5)         # tuning params based on size
+    
+	for (x,y,w,h) in faces: # only enters loop if non empty
+		cv2.rectangle(forHaar, (x,y), (x+w,y+h), (255,0,0), 2)
+		roi_gray = grayHaar[y:y+h, x:x+w]                           # create a smaller region inside faces to detect eyes --> won't find eye outside of face
+		roi_color = forHaar[y:y+h, x:x+w]
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (ex, ey, ew, eh) in eyes:
+			cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,0,255), 2)
+        
+	cv2.imshow('Haar Face Detection Algorithm', forHaar)
 
 	# if the frame could not be grabbed, then we have reached the end of the video
 	if frame is None:
@@ -31,7 +48,7 @@ while True:
 	frame = imutils.resize(frame, width=500)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+	
 	# if the first frame is None, initialize it
 	if firstFrame is None:
 		print(frame)
