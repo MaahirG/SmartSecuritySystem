e420985d9a59293d86b82a08d999cb5881af89a2
Maahir <maahir.gupta@gmail.com>
2020-05-23 16:35:17 -0400

    CNN implemented

--- smart_security.py
+++ smart_security.py
@@ -22,16 +22,33 @@ servo = GPIO.PWM(11,50)      # 50 is pulse rate (Hz)
 
 servo.start(7)
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# reading from webcam
-vs = VideoStream(src=0).start()
-time.sleep(1.0)
+face_cascade = cv2.CascadeClassifier('haarModels/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarModels/haarcascade_eye.xml')
 
-# initialize the first frame in the video stream
-firstFrame = None
-avg = None
+# loading mobilenet ssd models into opencv dnn library
+model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb', 'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+
+
+# Pretrained classes dictionary in the model
+classDict = {0: 'background',
+              1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
+              7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+              13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+              18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+              24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+              32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+              37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+              41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+              46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+              51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+              56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+              61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+              67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+              75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+              80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+              86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
+
 
 # for servo
 def SetAngle(angle):
@@ -42,37 +59,75 @@ def SetAngle(angle):
 	GPIO.output(11, False)
 	servo.ChangeDutyCycle(0)
 
+def idToClassName(class_id, classes):		#classes is a dictionary that holds all classnames
+	for key, value in classes.items():
+		if class_id == key:
+			return value	# returns actual class name -- dog, cat, human etc instead of a class_id (number)
+
+
+# Start reading from webcam
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
+
+# initialize the first frame in the video stream
+firstFrame = None
+avg = None
 
 # loop over the frames of the video
 while True:
 	# grab the current frame and initialize the occupied/unoccupied text
 	frame = vs.read()
 	text = "Unoccupied"
-	frame = imutils.resize(frame, width=625)
+	img = imutils.resize(frame, width=625)
 
-	# if the frame could not be grabbed, then we have reached the end of the video - used for non livestreaming
-	if frame is None:
-		break
+	# blob is like a tensor, a compact object of data (preprocessed image). need to feed into the net
+	# the specific model used relies on 300x300 input image for its architecture
+	# model relies on RGB input not BGR like opencv (swapRB)
+	model.setInput(cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True))
+	
+	# Network produces output blob with a shape 1x1xNx7 where N is a number of
+	# detections and every detection is a vector of values --> 7 is the size of each detection vector (7 prediction criterion)
+	# [batchId, classId, confidence, left, top, right, bottom]
+	output = model.forward()
+	image_height, image_width, _ = frame.shape
 
-	haarFrame = frame
+	for detection in output[0,0,:,:]:
+    	confidence = detection[2]		# 2 because confidence is in the 2 spot of the detection vector
+		
+		if confidence > 0.5:
+			classID = detection[1]
+			className = idToClassName(classID, classDict)
+			print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
+			
+			box_x = detection[3] * image_width # detections are % left etc.
+			box_y = detection[4] * image_height
+			box_width = detection[5] * image_width
+			box_height = detection[6] * image_height
+			cv2.rectangle(frame, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
+			cv2.putText(frame, className, (int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255))
+
+		if className == 'person':
+			text = "Occupied"
+
+	haarFrame = img
 	grayHaar = cv2.cvtColor(haarFrame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(grayHaar, 1.3, 5)         # tuning params based on size
 	
 	for (x,y,w,h) in faces: # only enters loop if non empty
 		print("x is: ", x, " y is: ", y, " w is: ", w, " h is: ", h)
 
-		if(x > 300):
-			SetAngle(180)
-		elif(x<=300 and x > 240):
-			SetAngle(144)	
-		elif(x<=240 and x > 180):
-			SetAngle(108)
-		elif(x<=180 and x > 120):
-			SetAngle(72)
-		elif(x<=120 and x > 60):
-			SetAngle(36)
-		elif(x<=60 and x >= 0):
-			SetAngle(0)
+		# if(x > 300):
+		# 	SetAngle(180)
+		# elif(x<=300 and x > 240):
+		# 	SetAngle(144)	
+		# elif(x<=240 and x > 180):
+		# 	SetAngle(108)
+		# elif(x<=180 and x > 120):
+		# 	SetAngle(72)
+		# elif(x<=120 and x > 60):
+		# 	SetAngle(36)
+		# elif(x<=60 and x >= 0):
+		# 	SetAngle(0)
 
 		cv2.rectangle(haarFrame, (x,y), (x+w,y+h), (255,0,0), 2)
 		roi_gray = grayHaar[y:y+h, x:x+w]                           # create a smaller region inside faces to detect eyes --> won't find eye outside of face
@@ -80,48 +135,7 @@ while True:
 		eyes = eye_cascade.detectMultiScale(roi_gray)
 		for (ex, ey, ew, eh) in eyes:
 			cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,0,255), 2)
-		
-	
-	# Blur the resized and grayscaled frame
-	gray = cv2.GaussianBlur(grayHaar, (25, 25), 0)
-	
-	# if the first frame is None, initialize it
-	if firstFrame is None:
-		firstFrame = gray
-		continue
 
-	# if the average frame is None, initialize it
-	if avg is None:
-		avg = gray.copy().astype("float")
-		continue
-
-	# Object Detection/Edge Detection
-
-	# accumulate the weighted average between the current frame and (accumulated)previous frames, then compute the difference between the current frame and running average
-	cv2.accumulateWeighted(firstFrame, avg, 0.5)	# avg is accumulated slight differences in pixels over the iteration of frames, 0.5 is weight for gray frame
-	frameDelta = cv2.absdiff(firstFrame, gray)  # element wise differences new array of differences in pixels
-
-	thresh = cv2.threshold(frameDelta, 55, 255, cv2.THRESH_BINARY)[1] # new array of thresheld (camera inaccuracies) differences in pixels returned as thresh
-	
-	# dilate the thresholded image to fill in holes, then find contours on thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=6)  # dilation fills in neighbouring pixels as 1 (for binary) to add pixels to the outer border (dilating pixels in the threshold array)
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)	# CHAIN_APPROX_SIMPLE takes away redundancies when highlighting the curves/joining parts of a threshold frameDelta
-	# cnts is a tuple - should have 2 things inside - a vector of vectors all separate contours (with xy vector of coords defining contour) and a vector of hierarchies
-	# once we grab contours (tuple of relevant x,y coords highlighting the curves joining a shape)
-	# print(type(cnts))
-	# print(cnts)
-	cnts = imutils.grab_contours(cnts) # grabs only the x,y coord tuples
-
-	# loop over the contours - loop through lists inside tuple containing respective contour x,y coords (depending on how many objects were detected separate) - if 1 object, one list in the tuple
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < 800:  	# area based on boundary coordqinates from contour
-			continue								# disregard but keep looping the rest of the contours
-		
-		# compute the bounding box for the contour, draw it on the frame, and update the text
-		(x, y, w, h) = cv2.boundingRect(c)								# gets rectangle dimensions based on entire list of contour coords
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
 
 
 	# draw the text and timestamp on the frame
@@ -131,10 +145,8 @@ while True:
 	cv2.imshow('Haar Face Detection Algorithm', haarFrame)
 
 	# show the frame and record if the user presses a key
-	cv2.imshow("Documented Background", firstFrame)
 	cv2.imshow("Security Feed", frame)
-	cv2.imshow("Threshold", thresh)
-	cv2.imshow("Frame Delta", frameDelta)
+	
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key is pressed, break from the lop
